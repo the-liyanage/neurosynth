23699c13abfb0110bdd978c1fb0f19f602feb9f9
Hiruni Liyanage <87710665+the-liyanage@users.noreply.github.com>
2026-07-27 18:40:50 +0530

    on this

--- config.py
+++ config.py
@@ -2,8 +2,11 @@ from pathlib import Path
 
 # paths
 ROOT_DIR = Path(__file__).parent
-RAW_DATA_DIR = ROOT_DIR/ "data" / "raw"
-PROCESSED_DATA_DIR = ROOT_DIR/ "data" / "processed"
+RAW_DATA_DIR            = ROOT_DIR/ "data" / "raw"
+PROCESSED_DATA_DIR      = ROOT_DIR/ "data" / "processed"
 
 
-ARTIFACTS_DIR = ROOT_DIR / "artifacts"
+ARTIFACTS_DIR           = ROOT_DIR/ "artifacts"
+MODEL_PATH              = ARTIFACTS_DIR/ "best_model.pth"
+CONFIG_PATH             = ARTIFACTS_DIR/ "model_config.json"
+
