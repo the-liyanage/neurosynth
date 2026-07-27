8f0ad88dd0e23b01a0fca13bcff94d50e45d50f9
Hiruni Liyanage <87710665+the-liyanage@users.noreply.github.com>
2026-07-28 00:45:45 +0530

    update preprocessing

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -57,3 +57,113 @@ def load_subject(subject, runs, data_dir):
     return raw
     
     
+# applying common average reference (CAR)
+def apply_car(raw):
+    """
+    removes noise shared across all electrodes simultaneously 
+    - validated during EDA where we found high-inter channel 
+    correlation due to volume conduction
+    
+    """
+    raw_car = raw.copy()
+    raw.set_eeg_reference("average", verbose = False)
+    return raw
+
+
+# applying bandpass filter
+def apply_bandpass_filter(raw):
+    """
+    keeps only 8-30 Hz the Alpha + Beta motor imagery bands.
+    validated during EDA with PSD plot showing real activity
+    in this range.
+    
+    below 8 Hz --> movement/sweat noise
+    above 30Hz --> muscle noise + power line interference
+    """
+    raw.filter(
+        l_freq = FREQ_LOW,
+        h_freq = FREG_HIGH,
+        # a filter (mathematical way)
+        method = "iir",
+        verbose = False
+    )
+    return raw
+
+
+# extract epochs
+def extract_epochs(raw):
+    """
+    cuts the continuous signal into 4-second windows
+    aligned to each T1/T2 event onset
+    
+    note : event_id passed to events_from_annotations to 
+    avoid the T0/T1/T2 label-mapping bug found during
+    development (TO was accidentally included before this fix)
+    """
+    events, _ = mne.events_from_annotations(
+        raw,
+        event_id = EVENT_ID,
+        verbose = False
+    )
+    
+    epochs = mne.Epochs(
+        raw,
+        events,
+        event_id = EVENT_ID,
+        tmin = EPOCH_TMIN,
+        tmax = EPPOCH_TMAX,
+        baseline = None,
+        preload = True,
+        verbose = False
+    )
+    
+    X = epochs.get_data()
+    y = epochs.events[:, 2] -1
+    return X, y    
+    
+    
+    
+# normalize
+def normalize(X):
+    """
+    z-score normalization per channel per epoch.
+    removes amplitude differences between subject caused by
+    skull thickness, hair, electrode contact quality.
+    
+    model learns signal PATTERNS not raw voltage values
+    
+    """
+    mean = X.mean(axis = -1, keepdims = True)
+    std  = X.std(axis = -1, keepdims = True)
+    std[std == 0] = 1
+    return (X - mean) /std
+    
+    
+# preprocess one raw EEG signal
+def preprocess_raw_signal(raw):
+    """
+    runs the full preprocessing pipeline on ONE raw
+    EEG recording - used by the serving layer for
+    real - time inference on incoming signals 
+    
+    Pipeline: CAR --> bandpass filter --> epoch ---> normalize
+    ---> transpose (time first, for Transfirner input)
+    """
+    
+    raw = apply_car(raw)
+    raw = apply_bandpass_filter(raw)
+    X, _ = extract_epochs(raw)
+    X = normalize
+    
+    # transpose from (n_epochs, 64, 641)
+    # to (n_epochs, 641, 64) for Transformer input
+    X = X.transpose(0, 2, 1)
+    return X
+
+
+# full pipeline for training data
+
+    
+        
+    
+    
