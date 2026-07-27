76dab1d27d66be259c26da8de601f9ae3b38d4ce
Hiruni Liyanage <87710665+the-liyanage@users.noreply.github.com>
2026-07-27 15:58:29 +0530

    modify structure

---
+++ config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# paths
+ROOT_DIR = Path(__file__).parent
+RAW_DATA_DIR = ROOT_DIR/ "data" / "raw"
+PROCESSED_DATA_DIR = ROOT_DIR/ "data" / "processed"
+
+
+ARTIFACTS_DIR = ROOT_DIR / "artifacts"
---
+++ src/data/preprocess.py
@@ -0,0 +1,6 @@
+import mne 
+import numpy as np
+from mne.datasets import eegbci
+from mne.io import concatenate_raws
+
+
