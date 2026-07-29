5f383abe0a82d731b6d82065db84e7a4bb93b213
Hiruni Liyanage <87710665+the-liyanage@users.noreply.github.com>
2026-07-29 18:02:00 +0530

    API Call

---
+++ src/serving/server.py
@@ -0,0 +1,48 @@
+# FastAPI server 
+# Exposes two endpoints:
+# GET /              ---> health check (confirms server is running)
+# POST / predict     ---> takes EEG signal, returns prediction
+# WS /  ws           ---> Websocket for real time inference
+
+
+
+import numpy as np
+from fastapi  import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+import json
+
+from src.serving.predict import predict, predict_from_raw
+from config import PORT, HOST, LABELS
+
+# Create FastAPI app
+app = FastAPI(
+    title = "NeuroSynth API",
+    description = "Real-time EEG motor imagery classification",
+    version = "1.0.0"
+)
+
+
+# CORS Middleware
+# CORS = Cross - Origin Resource Sharing
+# Without this, our React frontend (running on port 3000)
+# would be blocked from talking to this server (port 8000)
+# by the browser's security policy
+# allow_origins = ["*"] means accept requests from ANY origin
+# in production you'd restrict this to your specific frontend URL
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+
+# Request/Response models
+# Pydantic models define the exact shape of data
+# coming IN and going OUT of our API endpoints
+# FastAPI uses these to automatically valudate requests
+# and generate API documentation
+
+
